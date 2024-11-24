afff507a090febc088298d56ff852a67b97c34a6
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-24 14:07:03 -0300

    Umas horas de estudos

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,9 +1,37 @@
 let nome = "Marcos"
-const idade = "34"
+const idade = 34
 var cidade = "Recife"
 let hobby = "Animes"
+let temcarteira= true
+const a = 10
+const b = 3
+const x = 5
+const y = "5"
+
+console.log ("Soma:",a+b,)
+console.log("Subtrair:",a-b,)
+console.log("Dividir:",a/b,)
+console.log("Multiplicar:",a*b,)
+console.log("Resto:",a%b,)
+console.log(x==y)
+console.log(x===y)
+console.log(x!=y)
+console.log(x>y)
+console.log(x<y)
+console.log(x<=y)
+console.log(x>=y)
+
 console.log ("Olá,",nome,"! Você mora em ",cidade,", você tem ",idade," anos e você gosta de ",hobby,'!')
 
-console.log ("hello, javascript!");
-console.log ("vai que vai!")
+console.log(idade>="18"&&temcarteira)
+console.log(idade<"18"||temcarteira)
+console.log(!temcarteira)
+
+console.log("Minha idade é maior que 18 anos?",idade>"18",)
+console.log("Minha idade é menor que 18 anos?",idade!="18",)
+console.log("Minha idade é menor que 18 anos?",idade<"18",)
+console.log("Minha idade é menor que 18 anos?",idade!="35",)
+
+
+
 
