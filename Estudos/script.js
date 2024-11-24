90a5fefbf71c77cd9329478aedd9abc3f14facee
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-24 12:22:13 -0300

    Update script.js

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,8 +1,8 @@
-let nome = "Marcos"
-const Idade = "34"
-var cidade = "Recife"
-let Hobbby = "Animes"
-console.log (Olá "nome", Você mora em "+cidade+", você tem "idade" anos e você gosta de "habby"!)
+let nome = Marcos
+const Idade = 34
+var cidade = Recife
+let Hobbby = Animes
+console.log ("Olá," nome "! Você mora em "+cidade+", você tem "idade" anos e você gosta de "habby'!')
 
 console.log ("hello, javascript!");
 console.log ("vai que vai!")
