e5b6c66987e3130a2f71f83fd588522c58fa9392
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-24 12:16:49 -0300

    tentando o cod

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,2 +1,9 @@
+let nome = "Marcos"
+const Idade = "34"
+var cidade = "Recife"
+let Hobbby = "Animes"
+console.log (Olá "nome", Você mora em "+cidade+", você tem "idade" anos e você gosta de "habby"!)
+
 console.log ("hello, javascript!");
 console.log ("vai que vai!")
+
