35c8fd589ea73ba76388f9514931119b1952d4ea
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-25 03:27:40 -0300

    Tentando colocar na tela prompt

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,21 +1,9 @@
-let nota = 9
-let i=1
+function soma = prompt ("Digite aqui!")
 
-if (nota>=7){
-    console.log("Aprovado Krai!")
-} else if (nota>=5){
-    console.log("Tenta de Novo!")
+let resultado = soma()
+if (resultado%2===0){
+    console.log("O número",resultado,"é PAR!!")
 }else{
-    console.log("Que peninha!")
+    console.log("O número",resultado,"é IMPAR!!!")
 }
-
-for(let i=1; i<= 6; i++){
-    if (i%2===0){
-        console.log("Número:",i,"é PAR")
-    }else{
-        console.log("Número:",i,"é IMPAR")
-    }
-}
-
-
-
+      
