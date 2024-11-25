9968b9326bc98b6ad934655a6a6ca9be5f6df091
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-24 22:09:25 -0300

    Update script.js

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,8 +1,17 @@
+let nota = 9
 
+if (nota>=7){
+    console.log("Aprovado Krai!")
+} else if (nota>=5){
+    console.log("Tenta de Novo!")
+}else{
+    console.log("Que peninha!")
+}
 
+for (let i=1; i<=20; i++){
+    console.log("Número",i,)
+}
 
-
-
-
+if ()
 
 
