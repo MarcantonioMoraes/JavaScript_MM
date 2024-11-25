30e66d14d212d3cad963541abed99f27d107a6d5
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2024-11-24 21:34:05 -0300

    colocando o prompt

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,35 +1,5 @@
-let nota=7
-let dia=3
+let nome = prompt
 
-if (nota>=7){
-    console.log("Aprovado!")
-}else if (nota>=5){
-    console.log("Recuperação!")
-}else{
-    console.log("Reprovado!")
-}
-
-switch(dia){
-    case 1:
-        console.log("")
-        break
-        case 1:
-            console.log("Segunda!")
-            break
-        case 2:
-            console.log("Terça!")
-            break
-        case 3:
-            console.log("Quarta!")
-            break 
-        case 4:
-            console.log("Quinta!")
-            break            
-        case 5:
-            console.log("Sexta!")
-            break
-        default:
-            console.log("Final de Semana!")      
-}
+console.log(nome)
 
 
