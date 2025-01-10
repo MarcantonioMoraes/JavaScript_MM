804be3553418c7c1c17dcf2b6459d2512cb441f4
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2025-01-10 03:46:11 -0300

    Update script.js

--- Estudos/script.js
+++ Estudos/script.js
@@ -29,7 +29,7 @@ let temfilhos=true
 let quantosfilhos=2
 let temcarteira=true
 let casado=true
-let pessoa=1
+let pessoa=2
 
 console.log ("eu me chamo",nome,"tnho",idade,"nasci no mês de",mes,"moro na rua",rua,"filho de",mae,"e de ",pai,"casado com",esposa," e pai de",f1,"e",f2,". A diferença da minha idade com a da minha esposa é",idade%idesposa,"anos.")
 
@@ -45,6 +45,7 @@ console.log( c<d)
 console.log(d!=c )
 console.log(d>=c+a)
 console.log(c<=a+b)
+
 console.log(casado&&quantosfilhos>1)
 console.log(!casado )
 console.log(!casado||!temfilhos)
@@ -68,6 +69,32 @@ else{
   console.log("Cadê os meninos?")
 }
 
+switch(pessoa){
+  case 1:
+    console.log(nome);
+  break;
+  case 2:
+    console.log(esposa);
+  break;
+  case 3:
+    console.log(f1);
+  break;
+  case 4:
+    console.log(f2);
+  break;
+  default:
+    console.log("Não Conheço!")
+
+}
+
+for (let w=1;w<=5;w++){
+  console.log("Número",w);
+}
+
+while (a>=0){
+  console.log("Contador",a);
+  a--;
+}
 
 
 
