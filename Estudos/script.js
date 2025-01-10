8ce90fa0c7afb704d1f7db769e022dbd631b8561
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2025-01-10 02:33:47 -0300

    2025.1

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,3 +1,15 @@
+// && (E): Verdadeiro se todas as condições forem verdadeiras.
+// || (OU): Verdadeiro se qualquer condição for verdadeira.
+// ! (NÃO): Inverte o valor lógico.
+// == (igual a, verifica apenas o valor).
+// === (estritamente igual, verifica valor e tipo).
+// != (diferente).
+// > (maior que).
+// < (menor que).
+// >= (maior ou igual).
+// <= (menor ou igual).
+
+
 const nome = "Marcantonio"
 const idade = 35
 const mes = "Janeiro"
@@ -8,10 +20,14 @@ const esposa = "Raquel Thays"
 const idesposa= 30
 const f1="Thulio Marc"
 const f2="Thalys Marc"
-const a=6
-const b=4
-const c=24
-const d=54
+let a=6
+let b=4
+let c=24
+let d=54
+let temfilhos=true
+let quantosfilhos=2
+let temcarteira=true
+let casado=true
 
 console.log ("eu me chamo",nome,"tnho",idade,"nasci no mês de",mes,"moro na rua",rua,"filho de",mae,"e de ",pai,"casado com",esposa," e pai de",f1,"e",f2,". A diferença da minha idade com a da minha esposa é",idade%idesposa,"anos.")
 
@@ -20,4 +36,14 @@ console.log(d-b)
 console.log(d/a)
 console.log(c*b)
 console.log(d%c)
+
+console.log(a==b )
+console.log(a>b)
+console.log( c<d)
+console.log(d!=c )
+console.log(d>=c+a)
+console.log(c<=a+b)
+console.log(casado&&quantosfilhos>1)
+console.log(!casado )
+console.log(!casado||!temfilhos)
 
