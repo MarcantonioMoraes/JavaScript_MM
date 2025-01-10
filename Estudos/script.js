535f72fd2434f8d24f8de39a13dd25c9997d9c15
MarcantonioMoraes <marcantonio.lupercino@gmail.com>
2025-01-10 01:04:06 -0300

    2025

--- Estudos/script.js
+++ Estudos/script.js
@@ -1,22 +1,23 @@
-function isPrime(num) {
-    // Números menores ou iguais a 1 não são primos
-    if (num <= 1) {
-      return false;
-    }
-  
-    // Verifica se o número é divisível por qualquer número entre 2 e a raiz quadrada de 'num'
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-      if (num % i === 0) {
-        return false; // O número não é primo
-      }
-    }
-  
-    return true; // O número é primo
-  }
-  
-  // Testando a função com diferentes números
-  console.log(isPrime(7));  // true
-  console.log(isPrime(10)); // false
-  console.log(isPrime(1));  // false
-  console.log(isPrime(2));  // true
-  
+const nome = "Marcantonio"
+const idade = 35
+const mes = "Janeiro"
+const rua = "Rua Córrego do Deodato"
+const mae = "Edna Maria"
+const pai="Jose antonio"
+const esposa = "Raquel Thays"
+const idesposa= 30
+const f1="Thulio Marc"
+const f2="Thalys Marc"
+const a=6
+const b=4
+const c=24
+const d=54
+
+console.log ("eu me chamo",nome,"tnho",idade,"nasci no mês de",mes,"moro na rua",rua,"filho de",mae,"e de ",pai,"casado com",esposa," e pai de",f1,"e",f2,". A diferença da minha idade com a da minha esposa é",idade%idesposa,"anos.")
+
+console.log(a+c)
+console.log(d-b)
+console.log(d/a)
+console.log(c*b)
+console.log(d%c)
+
--- Estudos/variaveis.js
+++ Estudos/variaveis.js
@@ -107,3 +107,25 @@ console.log(resultadoo(42))
 console.log(resultadoo(13))
 
 
+//function isPrime(num) {
+    // Números menores ou iguais a 1 não são primos
+    if (num <= 1) {
+      return false;
+    }
+  
+    // Verifica se o número é divisível por qualquer número entre 2 e a raiz quadrada de 'num'
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false; // O número não é primo
+      }
+    }
+  
+    return true; // O número é primo
+  //}
+  
+  // Testando a função com diferentes números
+  console.log(isPrime(7));  // true
+  console.log(isPrime(10)); // false
+  console.log(isPrime(1));  // false
+  console.log(isPrime(2));  // true
+  
